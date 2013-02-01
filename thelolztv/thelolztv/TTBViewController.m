eb20fb824da490768bb6fe1f9692df7be7d35cf1
Jabari Bell <jabari.bell@pxlflu.net>
2013-01-31 21:49:16 -0500

    afnetworking added and search bar returning json with search item

--- thelolztv/thelolztv/TTBViewController.m
+++ thelolztv/thelolztv/TTBViewController.m
@@ -7,18 +7,22 @@
 //
 
 #import "TTBViewController.h"
+#import "AFJSONRequestOperation.h"
 
 @interface TTBViewController ()
 @property (strong, nonatomic) IBOutlet UITextField *searchField;
 @end
 
-@implementation TTBViewController
+@implementation TTBViewController {
+    NSOperationQueue *queue;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self.searchField addTarget:self action:@selector(textFieldFinished:) forControlEvents:UIControlEventEditingDidEndOnExit];
+    queue = [[NSOperationQueue alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,9 +31,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSURL *)urlWithSearchText
+{
+    NSString *urlString = [NSString stringWithFormat:@"https://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&q=%@&format=1", self.searchField.text];
+    return [NSURL URLWithString:urlString];
+}
+
+- (void)performSearch
+{
+    if (self.searchField.text.length > 0) {
+        NSLog(@"searching");
+        NSURLRequest *request = [NSURLRequest requestWithURL:[self urlWithSearchText]];
+        
+        AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON){
+            NSLog(@"results: %@", JSON);
+        }
+        failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON){
+            NSLog(@"fail whale: %@", error);
+        }];
+        
+        operation.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", nil];
+        
+        [queue addOperation:operation];
+    }
+}
+
 - (void)textFieldFinished:(id)sender
 {
-    NSLog(@"omg");
+    [self performSearch];
 }
 
 @end
